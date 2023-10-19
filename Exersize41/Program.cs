9d89e038a0e3f5c2154f705a06a9cd1a8637e282
Andrei Strakhov <straxarts@outlook.com>
2023-10-19 14:38:37 +0300

    выполнил задачу 41

---
+++ Exersize41/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+Console.Write("Введите количество вводимых чисел: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [M];
+Console.WriteLine(M);
+for (int i = 0; i <= M-1; i++)
+{
+    Console.Write($"Введите {i+1} число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"Массив введённых чисел: [{string.Join(", ", array)}]");
+int count = 0;
+for (int i = 0; i <= M-1; i++)
+{
+    if (array[i] > 0) count = count + 1;
+}
+Console.Write($"Количество положительных чисел: {count}");
